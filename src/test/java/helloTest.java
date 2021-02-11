c044daca3b7abaec41312faf23abfb4b6c4c8ea4
furkan <ozdemir1furkan@gmail.com>
2021-02-11 10:55:45 -0300

    this is just practice

---
+++ src/test/java/helloTest.java
@@ -0,0 +1,9 @@
+public class helloTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+    //This is our group project practice
+    //This is our group practice
+}
